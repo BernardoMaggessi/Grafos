076011fda9d7e85d8f53d82bba44de15ad2912f8
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-03-16 17:49:14 -0300

    main tests

--- src/app/Program.java
+++ src/app/Program.java
@@ -10,6 +10,13 @@ public class Program {
         grafo.addVertice("Pedro");
         grafo.addVertice("Kleber");
         grafo.addVertice("Phelipe");
+
+        grafo.addAresta(2.0,"João", "Lorenzo");
+        grafo.addAresta(3.0,"Lorenzo", "Kleber");
+        grafo.addAresta(1.0, "Kleber", "Pedro");
+        grafo.addAresta(2.0,"João", "Pedro");
+        grafo.addAresta(3.0, "Pedro", "Phelipe");
+        grafo.addAresta(2.0, "Phelipe", "Kleber");
     }
 
 
